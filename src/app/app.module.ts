1114221c73cde24fdaff0c8850327c8e5524cdd8
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-03-14 12:27:45 +0200

    add footer and navbar initial comonents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/sharedComponents/footer/footer.component';
+import { NavbarComponent } from './Components/sharedComponents/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
