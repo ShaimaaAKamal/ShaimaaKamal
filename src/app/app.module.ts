0d71e8d958be30ece49b38580589ed2a70be5064
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-03-25 17:45:48 +0200

    add components to declartions and import NgxParticlesModule to be abale to used particles in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,39 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxParticlesModule } from '@tsparticles/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './Components/sharedComponents/footer/footer.component';
 import { NavbarComponent } from './Components/sharedComponents/navbar/navbar.component';
+import { HomeComponent } from './Components/mainComponents/home/home.component';
+import { ResumeComponent } from './Components/mainComponents/resume/resume.component';
+import { PortfolioComponent } from './Components/mainComponents/portfolio/portfolio.component';
+import { ContactMeComponent } from './Components/mainComponents/contact-me/contact-me.component';
+import { ServicesComponent } from './Components/mainComponents/services/services.component';
+import { BlogsComponent } from './Components/mainComponents/blogs/blogs.component';
+import { TestimonialsComponent } from './Components/mainComponents/testimonials/testimonials.component';
+import { MovingCircleComponent } from './Components/mainComponents/moving-circle/moving-circle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
+    HomeComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ContactMeComponent,
+    ServicesComponent,
+    BlogsComponent,
+    TestimonialsComponent,
+    MovingCircleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    NgxParticlesModule  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
