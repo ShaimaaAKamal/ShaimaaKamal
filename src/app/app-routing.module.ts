a1c295867445f5fdc99225f1c05bb6f20b333b9a
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-03-26 17:03:00 +0200

    update routing with new routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/mainComponents/home/home.component';
+import { AboutComponent } from './Components/mainComponents/about/about.component';
+import { ContactMeComponent } from './Components/mainComponents/contact-me/contact-me.component';
+import { PortfolioComponent } from './Components/mainComponents/portfolio/portfolio.component';
+import { SkillsComponent } from './Components/mainComponents/skills/skills.component';
 
 const routes: Routes = [
-  {path:"",component:HomeComponent}
+  {path:"",component:HomeComponent,pathMatch:"full"},
+  {path:"About",component:AboutComponent},
+  {path:"Skills",component:SkillsComponent},
+  {path:"Projects",component:PortfolioComponent},
+  {path:"Contact",component:ContactMeComponent},
 ]
 ;
 
