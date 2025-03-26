d2ab5aa75872f696809b5f4f95921f9f3be61dbe
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-03-26 17:01:52 +0200

    implement initial skills and about components

--- src/app/Components/sharedComponents/navbar/navbar.component.ts
+++ src/app/Components/sharedComponents/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
-  
+
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+ navActive = false;
 
+ showNav(){
+    this.navActive = !this.navActive;
+ }
+
+//     toggleNav() {
+//         this.navActive = !this.navActive;
+//     }
 }
