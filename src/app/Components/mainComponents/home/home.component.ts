94980d6b45714d69519ccc2b81a5656919bc6e8e
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-03-31 02:40:35 +0200

    finish home component

--- src/app/Components/mainComponents/home/home.component.ts
+++ src/app/Components/mainComponents/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-// import { ISourceOptions, tsParticles} from '@tsparticles/engine';
-// import { loadSlim } from '@tsparticles/slim';
 import Typed from 'typed.js';
 
 @Component({
@@ -11,119 +9,6 @@ import Typed from 'typed.js';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
-//   particlesOptions: ISourceOptions = {
-//   particles: {
-//     number: {
-//       value: 10,
-//       density: {
-//         enable: false,
-//       },
-//     },
-//     color: {
-//       value: "#fff",
-//     },
-//     shape: {
-//       type: "circle",
-//       options: {
-//         polygon: {
-//           sides: 5,
-//         },
-//         image: {
-//           src: "img/github.svg",
-//           width: 100,
-//           height: 100,
-//         },
-//       },
-//     },
-//     opacity: {
-//       value: 0.2,
-//       animation: {
-//         enable: false,
-//         speed: 0.9,
-//         sync: true,
-//       },
-//     },
-//     size: {
-//       value: 10,
-//       animation: {
-//         enable: true,
-//         speed: 80,
-//         sync: false,
-//       },
-//     },
-//     links: {
-//       enable: false,
-//       distance: 176,
-//       color: "#ffffff",
-//       opacity: 0.4,
-//       width: 2,
-//     },
-//     move: {
-//       enable: true,
-//       speed: 15,
-//       direction: "bottom-right",
-//       random: true,
-//       straight: false,
-//       outModes: "out",
-//       attract: {
-//         enable: false,
-//         rotate: {
-//           x: 600,
-//           y: 1200,
-//         },
-//       },
-//     },
-//   },
-//   interactivity: {
-//     detectOn: "canvas",
-//     events: {
-//       onHover: {
-//         enable: true,
-//         mode: "grab",
-//       },
-//       onClick: {
-//         enable: true,
-//         mode: "push",
-//       },
-//       resize: {
-//         enable: true,
-//       },
-//     },
-//     modes: {
-//       grab: {
-//         distance: 400,
-//         links: {
-//           opacity: 0.5,
-//         },
-//       },
-//       bubble: {
-//         distance: 400,
-//         size: 4,
-//         duration: 0.3,
-//         opacity: 1,
-//         speed: 3,
-//       },
-//       repulse: {
-//         distance: 200,
-//         duration: 0.4,
-//       },
-//       push: {
-//         quantity: 4, // ✅ `particles_nb` → `quantity`
-//       },
-//       remove: {
-//         quantity: 2, // ✅ `particles_nb` → `quantity`
-//       },
-//     },
-//   },
-//   detectRetina: true,
-// };
-
-
-//   async ngOnInit(): Promise<void> {
-//     await this.initializeParticles();
-//   }
-
 
   ngAfterViewInit() {
     new Typed('.typing-element', {
@@ -138,16 +23,20 @@ export class HomeComponent {
     });
   }
 
-// async initializeParticles(): Promise<void> {
-//     await loadSlim(tsParticles); // Load the slim version of tsparticles
+  sendMail() {
+  window.location.href = 'mailto:shaimaa.a.kamal.m@gmail.com?subject=Inquiry&body=Hello, I would like to know more about your services.';
+  }
 
-//     // Correct way to load particles
-//     await tsParticles.load({
-//         id: "tsparticles",
-//         options: this.particlesOptions // Use the correct structure
-//     });
+  downloadCV() {
+  const link = document.createElement('a');
+  link.href = 'assets/Files/Shaimaa Kamal.pdf';
+  link.download = 'Shaimaa_Kamal_CV.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
-//     console.log("Particles initialized!");
+// openCV() {
+//   window.open('assets/Files/Shaimaa Kamal.pdf', '_blank');
 // }
-
 }
