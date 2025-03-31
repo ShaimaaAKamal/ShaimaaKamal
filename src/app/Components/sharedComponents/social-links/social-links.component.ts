902557d90131034e61c06633f16c9cbb06422b24
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-03-31 02:40:13 +0200

    define layout
    adjust spacing across different screens

--- src/app/Components/sharedComponents/social-links/social-links.component.ts
+++ src/app/Components/sharedComponents/social-links/social-links.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-social-links',
   standalone: false,
-  
+
   templateUrl: './social-links.component.html',
   styleUrl: './social-links.component.scss'
 })
 export class SocialLinksComponent {
-
+sendWhatsAppMessage() {
+  const phoneNumber = '201020414320';
+  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent('')}`;
+  window.open(url, '_blank');
+}
 }
